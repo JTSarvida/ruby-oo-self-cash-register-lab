3c170bf43015a1befe278e732168fe0ed22936d5
jtsarvida <jtsarvida@gmail.com>
2022-07-12 14:06:07 -0400

    commit tree 4b477f94226622b356c719f4d859edfbe2edd27e

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+    
+    attr_accessor :total, :title, :price
+    attr_reader :discount
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item(title, price, quantity=0)
+        @quantity = quantity
+        if quantity != 0
+            price *= quantity
+        end
+        @total += price
+    end
+
+    def apply_discount
+        return self.discount
+    end
+end
 
